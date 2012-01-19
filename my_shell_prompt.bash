e9c072643bf7fe710a1a5d3a78469e1b88bebdac
Jason May <jmay@pobox.com>
2012-01-19 13:16:53 -0800

    smileys, learning about readline & non-printing chars, passing stuff between shell & functions

--- my_shell_prompt.bash
+++ my_shell_prompt.bash
@@ -4,11 +4,31 @@
 #
 # include hostname, current git repo status
 
+# colors
+# https://wiki.archlinux.org/index.php/Color_Bash_Prompt
+color_red=`echo -e "\033[0;31m"`
+color_green=`echo -e "\033[0;32m"`
+color_reset=`echo -e "\033[0m"`
+
+function status_sigil {
+  if [[ $1 -eq 0 ]]
+  then
+    sigil=☺
+    sigilcolor=$color_green
+  else
+    sigil=☹
+    sigilcolor=$color_red
+  fi
+}
+
 # 071214 JWM: reset terminal window title every time shell prompt is displayed
 # http://www.macosxhints.com/article.php?story=20031015173932306&query=bookmarks
 # TODO: only change prompt when you change directory?
 # executes this immediately before displaying $PS1
-export PROMPT_COMMAND='echo -ne "\033]0;${HOSTNAME%%.*}: "; basename "${PWD}"; echo -ne "\007"'
+export PROMPT_COMMAND='status_sigil $?;\
+  echo -ne "\033]0;${HOSTNAME%%.*}: ";\
+  basename "${PWD}";\
+  echo -ne "\007"'
 
 # 090327 http://www.hasmanybeers.com/2009/03/custom-bash-prompt-for-git-branches.html
 # adds the current git branch to the command prompt
@@ -22,4 +42,9 @@ function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
 }
 
-export PS1='\h:\W$(__git_ps1 "[\[\e[0;32m\]%s\[\e[0m\]\[\e[0;33m\]$(parse_git_dirty)\[\e[0m\]]")$ '
+
+# prompt should look like:
+# smiley-space-hostname-colon-basename-{[-branch-dirty-]} where the branch/dirty bits are only shown for git directories
+# \[ and \] delimiters are needed around non-printing sections of the prompt, otherwise readline gets confused
+
+export PS1='\[${sigilcolor}\]${sigil}\[${color_reset}\] \h:\W$(__git_ps1 "[\[${color_green}\]%s\[${color_red}\]$(parse_git_dirty)\[${color_reset}\]]")$ '
